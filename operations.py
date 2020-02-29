13eeef9adb2108488188f5f20e6b9b2c6bee24c9
anshu1515 <anshubansal595@gmail.com>
2020-02-29 17:55:35 +0530

    Operators

---
+++ operations.py
@@ -0,0 +1,16 @@
+x=eval(input("enter any number:"))
+y=eval(input("enter any number:"))
+c=x+y
+d=x-y
+e=x/y
+f=x*y
+z=x**y
+t=x%y
+s=x//y
+print(c)
+print(d)
+print(e)
+print(f)
+print(z)
+print(t)
+print(s)
